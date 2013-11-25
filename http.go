f9b4b53e109348b7561216dabc1c9bdae124b88f
Andrew Gerrand <adg@golang.org>
2013-11-25 15:19:16 +1100

    gofmt

--- http.go
+++ http.go
@@ -1,46 +1,46 @@
 package main
 
 import (
-  "net/http"
-"time"
-"path/filepath"
-  "fmt"
-  "io"
-  "os"
-  "log"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"path/filepath"
+	"time"
 )
 
 func main() {
-  http.HandleFunc("/save", saveFunc)
-  http.Handle("/", http.FileServer(http.Dir("static")))
-  log.Fatal(http.ListenAndServe(os.Args[1], nil))
+	http.HandleFunc("/save", saveFunc)
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	log.Fatal(http.ListenAndServe(os.Args[1], nil))
 }
 
 func saveFunc(w http.ResponseWriter, r *http.Request) {
-  mr, err := r.MultipartReader()
-  if err != nil {
-    http.Error(w, "error parsing multipart form", 500)
-  }
-  dir := fmt.Sprintf("%d", time.Now().Unix())
-  if err := os.Mkdir(dir, 0777); err != nil {
-    http.Error(w, fmt.Sprintf("couldn't write directory %s", dir), 500)
-  }
-  for {
-    p, err := mr.NextPart()
-    if err == io.EOF {
-      break
-    }
-    if err != nil {
-      http.Error(w, fmt.Sprintf("couldn't read file: %v", err), 500)
-    }
-    defer p.Close()
-    f, err := os.Create(filepath.Join(dir, p.FileName()))
-    if err != nil {
-      http.Error(w, fmt.Sprintf("couldn't create file: %v", err), 500)
-    }
-    if _, err := io.Copy(f, p); err != nil {
-      http.Error(w, fmt.Sprintf("couldn't write file: %v", err), 500)
-    }
-  }
-  fmt.Fprintf(w, "all done!")
+	mr, err := r.MultipartReader()
+	if err != nil {
+		http.Error(w, "error parsing multipart form", 500)
+	}
+	dir := fmt.Sprintf("%d", time.Now().Unix())
+	if err := os.Mkdir(dir, 0777); err != nil {
+		http.Error(w, fmt.Sprintf("couldn't write directory %s", dir), 500)
+	}
+	for {
+		p, err := mr.NextPart()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			http.Error(w, fmt.Sprintf("couldn't read file: %v", err), 500)
+		}
+		defer p.Close()
+		f, err := os.Create(filepath.Join(dir, p.FileName()))
+		if err != nil {
+			http.Error(w, fmt.Sprintf("couldn't create file: %v", err), 500)
+		}
+		if _, err := io.Copy(f, p); err != nil {
+			http.Error(w, fmt.Sprintf("couldn't write file: %v", err), 500)
+		}
+	}
+	fmt.Fprintf(w, "all done!")
 }
